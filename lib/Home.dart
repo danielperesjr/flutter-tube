3630bff9429faf70d97d52d3c5f6ecd5a56eb148
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-11 18:31:20 -0300

    Adicionada estrutura inicial da tela principal

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,8 +8,81 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    List<Widget> telas = [
+      Text("Início"),
+      Text("Em alta"),
+      Text("Inscrições"),
+      Text("Biblioteca"),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.grey,
+          opacity: 1
+        ),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+            "images/flutter-logo.png",
+          width: 88,
+          height: 25,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.videocam),
+              onPressed: (){
+                print("Ação: videocam");
+              },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              print("Ação: search");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: (){
+              print("Ação: conta");
+            },
+          )
+        ],
+      ),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (index){
+          setState(() {
+            _indiceAtual = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.blue,
+        items: [
+          BottomNavigationBarItem(
+            label: "Início",
+              icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+              label: "Em alta",
+              icon: Icon(Icons.whatshot)
+          ),
+          BottomNavigationBarItem(
+              label: "Inscrições",
+              icon: Icon(Icons.subscriptions)
+          ),
+          BottomNavigationBarItem(
+              label: "Biblioteca",
+              icon: Icon(Icons.folder)
+          ),
+        ],
+      ),
+    );
   }
 }
